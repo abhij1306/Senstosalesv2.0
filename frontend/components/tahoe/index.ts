145fbea9edf80ed62d1758b766296088081ed856
abhij1306 <abhij1306@gmail.com>
2025-12-31 22:39:31 +0530

    feat: Add TahoeButton component with macOS design

---
+++ frontend/components/tahoe/index.ts
@@ -0,0 +1,6 @@
+export { TahoeButton } from "./atoms/TahoeButton";
+
+// Export tokens and utilities for use in custom components
+export * from "./tokens";
+export * from "./utils";
+export * from "./motion";
